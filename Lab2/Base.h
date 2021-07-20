a1a081be35a8542fb0a7ca4ad97462c8d99bef07
Andreab-git <andrea01.bena@student.unife.it>
2021-07-20 18:45:59 +0200

    Updated

--- Lab2/Base.h
+++ Lab2/Base.h
@@ -16,24 +16,26 @@
 using namespace std;
 
 enum MType {
-    TDir=0, TFile=1
+    TDir = 0, TFile = 1
 };
 
 class Base {
 protected:
     string name;
 public:
-    string getName() const{
+    string getName() const {
         return name;
     };
+
     virtual int mType() const = 0;
-    virtual void ls(uint indent=0) const  = 0;
+
+    virtual void ls(uint indent = 0) const = 0;
 };
 
-class File: public Base {
-    uintmax_t  size;
+class File : public Base {
+    uintmax_t size;
 public:
-    File(string name, uintmax_t size): size(size){
+    File(string name, uintmax_t size) : size(size) {
         this->name = name;
     }
 
@@ -41,20 +43,25 @@ public:
         return TFile;
     }
 
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
     void ls(uint indent) const override {
-        for(int i=0; i<indent; i++){
-            cout<<" ";
+        for (int i = 0; i < indent; i++) {
+            cout << " ";
         }
-        cout<<name<<" "<<size<<endl;
+        cout << name << " " << size << endl;
     };
 
 
 };
 
-class Directory: public Base {
+class Directory : public Base {
 
     static shared_ptr<Directory> root;
-    list<shared_ptr<Base>> children;
+    list <shared_ptr<Base>> children;
 
     weak_ptr<Directory> self;
     weak_ptr<Directory> parent;
@@ -63,93 +70,149 @@ class Directory: public Base {
         this->name = name;
     }
 
-    static shared_ptr<Directory> make(string name, weak_ptr<Directory> parent){
-        shared_ptr<Directory> d = shared_ptr<Directory>(new Directory(name));
-        d->self = d;
-        d->parent = parent.lock() != nullptr ? parent: d;
-        return d;
+    /** <h3>Descrizione</h3>
+     *
+     *  Chiama il costruttore di <i>Directory</i> per generare un nuovo shared_ptr ad una directory di nome <i>name</i>.<br>
+     *  Salva il valore del puntatore in una variabile interna alla classe. Se il parametro <i>parent</i>
+     *  e' diverso da null, allora assegno il valore del puntatore (che rappresenta il padre)
+     *  alla variabile <i>parent<i/>.<br>
+     *
+     * @param name: Il nome della directory che si vuole creare
+     * @param parent: Il puntatore al padre della directory che si vuole creare
+     * @return shared_ptr di tipo Directory
+     */
+    static shared_ptr<Directory> make_dir(string name, weak_ptr<Directory> parent) {
+        shared_ptr<Directory> dir = shared_ptr<Directory>(new Directory(name));
+        dir->self = dir;
+        dir->parent = parent.lock() != nullptr ? parent : dir;
+        return dir;
     }
 
 public:
-    static void ciao() {cout<<"aaa";}
 
     /** <h3>Descrizione</h3>
      *  Restituisce la radice.<br>
-     *  Se la radice e' un nullptr, allora invoca il metodo <i>make</i> che crea la directory
+     *  Se la radice e' un nullptr, allora invoca il metodo <i>make_dir</i> che crea la directory
      *  assegnando il nome '/' ed un weak_ptr a <i>nullptr</i>
      * @return shared_ptr di tipo Directory
      */
-    static shared_ptr<Directory> getRoot(){
-        if(root == nullptr) {
-            root = make("/", shared_ptr<Directory>(nullptr));
+    static shared_ptr<Directory> getRoot() {
+        if (root == nullptr) {
+            root = make_dir("/", shared_ptr<Directory>(nullptr));
         }
         return root;
     }
 
-    std::shared_ptr<Directory> addDirectory(string name) {
-        shared_ptr<Directory> d = make(name, self);
-        children.push_back(d);
-        return d;
+    /** <h3>Descrizione</h3>
+     *  Aggiunge una directory come figlio, se la directory esiste, restituisce il puntatore
+     *  alla directory trovata.<br>
+     *
+     * @param name: Il nome della directory che si vuole creare
+     * @return shared_ptr alla directory creata
+     */
+    shared_ptr<Directory> addDirectory(const string name) {
+        // Crea una nuova directory
+        shared_ptr<Directory> dir = make_dir(name, self);
+        // Salva il puntatore nella lista contenente i figli
+        children.push_back(dir);
+        return dir;
     }
 
+    /** <h3>Descrizione</h3>
+     *
+     *  .<br>
+     *
+     * @return
+     */
     shared_ptr<File> addFile(string name, uintmax_t size) {
         shared_ptr<File> f = make_shared<File>(name, size);
         children.push_back(f);
         return f;
     }
 
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
     shared_ptr<Base> get(std::string name) {
-        if(name == "."){
+        if (name == ".") {
             return this->self.lock();
         }
 
-        if(name == ".."){
+        if (name == "..") {
             return this->parent.lock();
         }
 
-        for(auto c: children){
-            if(c->getName() == name){
+        for (auto c: children) {
+            if (c->getName() == name) {
                 return c;
             }
         }
         return shared_ptr<Base>(nullptr);
     }
 
+
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
     shared_ptr<Directory> getDir(string name) {
         shared_ptr<Base> dir = this->get(name);
-        if(dir == nullptr || dir->mType() != TDir){
+        if (dir == nullptr || dir->mType() != TDir) {
             return shared_ptr<Directory>(nullptr);
         } else {
             return dynamic_pointer_cast<Directory>(dir);
         }
     };
 
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
     shared_ptr<File> getFile(string name) {
         shared_ptr<Base> file = this->get(name);
-        if(file == nullptr || file->mType() != TFile){
+        if (file == nullptr || file->mType() != TFile) {
             return shared_ptr<File>(nullptr);
         } else {
             return dynamic_pointer_cast<File>(file);
         }
     };
 
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
     void remove(string name) {
         shared_ptr<Base> target = this->get(name);
-        if(target != nullptr) {
+        if (target != nullptr) {
             children.remove(target);
         }
     }
 
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
     int mType() const override {
-       return TDir;
+        return TDir;
     }
 
-    void ls(uint indent=0) const  override {
-        for(int i=0; i<indent; i++)
-            std::cout<<" ";
-        std::cout<<"+ "<<name<<endl;
-        for(auto child: children){
-            child->ls(indent+4);
+    /** <h3>Descrizione</h3>
+     *  .<br>
+     *
+     * @return
+     */
+    void ls(uint indent = 0) const override {
+        for (int i = 0; i < indent; i++)
+            std::cout << " ";
+        std::cout << "+ " << name << endl;
+        for (auto child: children) {
+            child->ls(indent + 4);
         }
     }
 
--- Lab2/main2.cpp
+++ Lab2/main2.cpp
@@ -9,9 +9,11 @@ shared_ptr<Directory> Directory::root = nullptr;
 int main() {
   // Chiamando getRoot() si ottiene uno shared_ptr di tipo Directory
   // Inizialmente root e' uguale a nullptr. Se e' uguale a nullptr, allora
-  // si imposta la directory di partenza assegnandogli il nome '/' ed il padre
-  // sara' uguale a nullptr.
+  // si imposta la directory di partenza assegnandogli il nome '/' ed il puntatore
+  // al padre della directory puntera' a nullptr.
   shared_ptr<Directory> root = Directory::getRoot();
+  // Aggiunge la directory ma se esiste, restituisce il puntatore alla directory
+  // trovata.
   auto alfa = root->addDirectory("alfa");
   root->addDirectory("beta")->addDirectory("beta1");
   root->getDir("beta")->addDirectory("beta2");
